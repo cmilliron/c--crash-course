1d7ee18726d72d0d348dce43aec1ccee8b86b6d4
Cody Milliron <codymilliron.dev@gmail.com>
2025-02-16 22:48:13 -0500

    Learning how to use functions

--- 01-up-and-running/playground.cpp
+++ 01-up-and-running/playground.cpp
@@ -1,5 +1,16 @@
 #include <cstdio>
 
+int step_function(int test_value) {
+    int result = 0;
+    if (test_value < 0) {
+        result = -1;
+    } else if (test_value > 0) {
+        result = 1;
+    }
+
+    return result;
+}
+
 int main() {
     int the_answer = 42;
     int lucky_number = the_answer / 6;
@@ -10,6 +21,18 @@ int main() {
     printf("42 is %d to x\n", equality);
     bool equality_1 = 0 == x;
     printf("42 is %d to x\n", equality_1);
+    
+    int x1 = 1;
+    if (x1 > 0) printf("Positive.\n");
+    else if (x1 < 0) printf("Negative.\n");
+    else printf("Zero.\n");
+    
+    int value1 = step_function(100); // value1 is  1
+    int value2 = step_function(0);   // value2 is  0
+    int value3 = step_function(-10); // value3 is -1
+    printf("Test  step fucntion: %d, %d, and %d.\n", value1, value2, value3);
 
+
+    
     return 0;
 }
